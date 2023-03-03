d6b6f0e39288eea8e0d37e6337690c33694ebc45
Rafael Dourado <rafael@sobit.com.br>
2023-03-03 16:21:35 -0300

    Seção 2: finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -12,28 +12,28 @@ class _PerguntaAppState extends State<PerguntaApp> {
     {
       'texto': 'Qual é a sua cor favorita ?',
       'respostas': [
-        {'texto': 'Preto', 'pontuação': 10},
-        {'texto': 'Vermelho', 'pontuação': 5},
-        {'texto': 'Verde', 'pontuação': 3},
-        {'texto': 'Branco', 'pontuação': 1},
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 3},
+        {'texto': 'Branco', 'pontuacao': 1},
       ],
     },
     {
       'texto': 'Qual é o seu animal favorito ?',
       'respostas': [
-        {'texto': 'Coelho', 'pontuação': 10},
-        {'texto': 'Cobra', 'pontuação': 5},
-        {'texto': 'Elefante', 'pontuação': 3},
-        {'texto': 'Leão', 'pontuação': 1}
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 5},
+        {'texto': 'Elefante', 'pontuacao': 3},
+        {'texto': 'Leão', 'pontuacao': 1}
       ],
     },
     {
       'texto': 'Qual é o seu instrutor favorito ?',
       'respostas': [
-        {'texto': 'Maria', 'pontuação': 10},
-        {'texto': 'João', 'pontuação': 5},
-        {'texto': 'Leo', 'pontuação': 3},
-        {'texto': 'Pedro', 'pontuação': 1},
+        {'texto': 'Maria', 'pontuacao': 10},
+        {'texto': 'João', 'pontuacao': 5},
+        {'texto': 'Leo', 'pontuacao': 3},
+        {'texto': 'Pedro', 'pontuacao': 1},
       ],
     },
   ];
@@ -45,7 +45,13 @@ class _PerguntaAppState extends State<PerguntaApp> {
         _pontuacaoTotal += pontuacao;
       });
     }
-    print(_pontuacaoTotal);
+  }
+
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
   }
 
   bool get temPergutnasSelecionadas {
@@ -72,7 +78,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quantResponder: _responder,
               )
-            : Resultado(),
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns!';
+    } else if (pontuacao < 12) {
+      return 'Você é bom!';
+    } else if (pontuacao < 16) {
+      return 'Impresionante!';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Parabéns!',
-        style: TextStyle(fontSize: 32),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 32),
+          ),
+        ),
+        TextButton(
+          child: Text('Reiniciar ?'),
+          style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
+              textStyle: MaterialStateProperty.all<TextStyle>(
+                  TextStyle(fontSize: 18))),
+          onPressed: quandoReiniciarQuestionario,
+        ),
+      ],
     );
   }
 }
